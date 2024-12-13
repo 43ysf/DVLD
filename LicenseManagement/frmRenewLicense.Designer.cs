4fa7a7855cd6580e25e7dcb01815193e70e70e09
Yousif Ahmed <76800385+43ysf@users.noreply.github.com>
2024-12-13 12:39:53 +0300

    Updates

--- LicenseManagement/frmRenewLicense.Designer.cs
+++ LicenseManagement/frmRenewLicense.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(582, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // frmRenewLicense
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1186, 869);
+            this.Controls.Add(this.label1);
             this.Name = "frmRenewLicense";
             this.Text = "frmRenewLicense";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
